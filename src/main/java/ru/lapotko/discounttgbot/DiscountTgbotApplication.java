32278ecd26d2de15d3c2c1999321084f7ee52e52
Danil <dlyaregistr123@bk.ru>
2021-06-05 22:05:00 +0700

    initial commit

---
+++ src/main/java/ru/lapotko/discounttgbot/DiscountTgbotApplication.java
@@ -0,0 +1,13 @@
+package ru.lapotko.discounttgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountTgbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscountTgbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lapotko/discounttgbot/DiscountTgbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lapotko.discounttgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountTgbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
